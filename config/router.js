bb0d93c3d3f0549cb2d2b6bc47b47079b0337c06
JennyJudova <jjudova@gmail.com>
2020-01-13 14:14:09 +0000

    user routers added and user added to evil plan model

--- config/router.js
+++ config/router.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
 const evilplans = require('../controllers/evilplans');
+const users = require('../controllers/auth');
 
 router
   .route('/evilplans')
   .get(evilplans.index)
   .post(evilplans.create); // secure route
+
+router.route('/register').post(users.register);
+
+router.route('/login').post(users.login);
+
+router.route('/profile').get(users.profile);
 
 module.exports = router; // exporting our router module for use in index.js
--- controllers/evilplans.js
+++ controllers/evilplans.js
@@ -3,7 +3,7 @@ const Evilplan = require('../models/Evilplan'); // we need the Animal models, at
 // index route GET - /animals
 function index(req, res) {
   Evilplan.find() // finds all the plans
-    // .populate('user')
+    .populate('user')
     // .populate('comments.user')
     .then((plans) => res.status(200).json(plans)) // if found, sends back the plans in an JSON array
     .catch(() => res.status(404).json({ message: 'Not Found index function' })); // if any error, sends back 404 not found message
@@ -11,9 +11,9 @@ function index(req, res) {
 
 // create route - POST to /animals
 function create(req, res, next) {
-  // console.log(req.currentUser);
+  console.log(req.currentUser);
   console.log(req.body);
-  // req.body.user = req.currentUser //attaching a user key to the body, making it values currentUser from secureRoute
+  req.body.user = req.currentUser; // attaching a user key to the body, making it values currentUser from secureRoute
   Evilplan.create(req.body) // creates a new animal based on the JSON object sent as the body of the request
     .then((plan) => res.status(201).json(plan)) // if it succesfully creates, sends back that new animal
     .catch(next); // otherwise we send the errors
