6db845eab848caf23f4f439d419b5b680cd089b7
JennyJudova <jjudova@gmail.com>
2020-01-16 16:13:02 +0000

    front end connected to the backend

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log('hi');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import './styles/style.scss';
+import Home from './components/common/home';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </main>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
