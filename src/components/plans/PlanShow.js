81d579b7bdf0202429d79d3fddb56c7fe35196e0
JennyJudova <jjudova@gmail.com>
2020-01-17 23:44:38 +0200

    PlanShow added

--- src/components/plans/PlanShow.js
+++ src/components/plans/PlanShow.js
@@ -2,16 +2,26 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-export default function PlanShow() {
-  const setData = (props) => {
-    const { id } = props.match.params;
+export default function PlanShow(props) {
+  const { id } = props.match.params;
+  console.log({ id });
+
+  const setData = () => {
+    console.log('id', id);
     axios
-      .get(`/api/evilplans/${id}`)
+      .get(`http://localhost:8000/api/evilplans/${id}`)
       .then((res) => console.log(res.data))
       .catch((err) => console.log(err));
   };
 
   useEffect(() => {
+    console.log('3');
     setData();
   }, []);
+
+  return (
+    <div>
+      <h2>Plan show</h2>
+    </div>
+  );
 }
